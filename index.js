9dd99bc5481266e2328b979b540b5df73d5a82b0
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-29 00:05:58 +0200

    feat(bot): add initial implementation

---
+++ index.js
@@ -0,0 +1,262 @@
+const logPayload = () => {};
+
+/*
+const fs = require('fs');
+
+const names = {};
+
+function logPayload(name, content) {
+
+  const count = names[name] || 0;
+
+  const suffix = count ? `_${count}`: '';
+
+  const fileName = `${__dirname}/node_modules/${name}${suffix}.json`;
+
+  fs.writeFileSync(fileName, JSON.stringify(content, null, '  '), 'utf-8');
+
+  names[name] = count + 1;
+}
+*/
+
+
+/**
+ * Main entry point of the <merge-me> probot.
+ *
+ * @param {import('probot').Application} app - Probot's Application class.
+ */
+module.exports = app => {
+
+  function getRepoAndOwner(repository) {
+    const repo = repository.name;
+    const owner = repository.owner.login;
+
+    return {
+      repo,
+      owner
+    };
+  }
+
+  async function findPullRequestByStatus(context, status) {
+
+    logPayload('status', status);
+
+    const {
+      sha,
+      repository,
+      branches,
+      state
+    } = status;
+
+    const {
+      repo,
+      owner
+    } = getRepoAndOwner(repository);
+
+
+    if (state !== 'success') {
+      context.log(`skipping: status == ${state}`);
+      return null;
+    }
+
+    const branch = branches.find((branch) => {
+      return branch.commit.sha === sha;
+    });
+
+
+    // check if PR
+    if (!branch) {
+      context.log('skipping: no branch matches ref');
+      return null;
+    }
+
+    context.log.debug(`checking branch ${branch.name}`);
+
+
+    // https://octokit.github.io/rest.js/#api-PullRequests-getAll
+    const {
+      data: pullRequests
+    } = await context.github.pullRequests.getAll({
+      owner,
+      repo,
+      ref: `${repository.name}:${branch.name}`
+    });
+
+    context.log.debug(`found ${pullRequests.length} pulls`);
+
+    logPayload('pulls.getAll', pullRequests);
+
+    if (!pullRequests.length) {
+      context.log('skipping: no PR matches ref');
+      return null;
+    }
+
+    return pullRequests[0];
+  }
+
+  async function checkMerge(context, pullRequest) {
+
+    const {
+      number,
+      head,
+      base
+    } = pullRequest;
+
+    const {
+      repo,
+      owner
+    } = getRepoAndOwner(base.repo);
+
+    const sha = head.sha;
+
+    context.log.debug(`checking merge on PR #${number}`);
+
+    // https://octokit.github.io/rest.js/#api-Repos-getProtectedBranchRequiredStatusChecks
+    const {
+      data: requiredStatusChecks
+    } = await context.github.repos.getProtectedBranchRequiredStatusChecksContexts({
+      owner,
+      repo,
+      branch: base.ref
+    });
+
+    logPayload('repos.branchRestrictions', requiredStatusChecks);
+
+    if (requiredStatusChecks.length) {
+      context.log.debug('validating merge against branch restrictions');
+    } else {
+      context.log.debug('validating merge against via all status checks');
+    }
+
+    const canMerge = requiredStatusChecks.length ? (summary) => {
+
+      return summary.state === 'success' || summary.statuses.every(function(status) {
+
+        const {
+          state,
+          context
+        } = status;
+
+        // wait for all checks to complete
+        if (state === 'pending') {
+          return false;
+        }
+
+        const isRequired = requiredStatusChecks.some((ctx) => {
+          return context === ctx || context.startsWith(`${ctx}/`);
+        });
+
+        return !isRequired || state === 'success';
+      });
+    } : (summary) => {
+      return summary.state === 'success';
+    };
+
+
+    // https://octokit.github.io/rest.js/#api-Repos-getCombinedStatusForRef
+    const {
+      data: status
+    } = await context.github.repos.getCombinedStatusForRef({
+      owner,
+      repo,
+      ref: sha
+    });
+
+    context.log.debug(`branch status ${status.state}`);
+
+    logPayload('repos.combinedStatus', status);
+
+    if (!canMerge(status)) {
+      context.log('skipping: ref merge rejected via status check');
+
+      return;
+    }
+
+
+    try {
+      // https://octokit.github.io/rest.js/#api-PullRequests-merge
+      const {
+        data: result
+      } = await context.github.pullRequests.merge({
+        owner,
+        repo,
+        number,
+        sha,
+        merge_method: 'rebase'
+      });
+
+      context.log.debug(`merged PR #${number}`);
+
+      logPayload('pulls.merge', result);
+    } catch (e) {
+      logPayload('pulls.mergeFail', e);
+
+      if (e.code === 405) {
+        const err = JSON.parse(e.message);
+
+        // TODO(nikku): print CANNOT AUTOMATICALLY MERGE to user (?)
+        context.log(`merge #${number} failed: ${err.message}`);
+
+        return;
+      }
+
+      context.log.error('merge failed', e);
+    }
+  }
+
+
+  app.on('pull_request_review.submitted', async context => {
+    logPayload('pullRequestReview.submitted', context.payload);
+
+    const {
+      review,
+      pull_request
+    } = context.payload;
+
+    if (review.state !== 'approved') {
+      context.log(`skipping: review in state ${review.state}`);
+
+      return;
+    }
+
+    // check, whether PR can be merged
+    return checkMerge(context, pull_request);
+  });
+
+
+  app.on('pull_request.opened', async context => {
+    logPayload('pullRequest.opened', context.payload);
+
+    // check, whether PR can be merged
+    return checkMerge(context, context.payload.pull_request);
+  });
+
+
+  app.on('pull_request.reopened', async context => {
+    logPayload('pullRequest.reopened', context.payload);
+
+    // check, whether PR can be merged
+    return checkMerge(context, context.payload.pull_request);
+  });
+
+
+  app.on('pull_request.synchronize', async context => {
+    logPayload('pullRequest.synchronize', context.payload);
+
+    // check, whether PR can be merged
+    return checkMerge(context, context.payload.pull_request);
+  });
+
+
+  app.on('status', async context => {
+    const pullRequest = await findPullRequestByStatus(context, context.payload);
+
+    if (!pullRequest) {
+      return;
+    }
+
+    // check, whether PR can be merged
+    return checkMerge(context, pullRequest);
+  });
+
+};
---
+++ test/index.test.js
@@ -0,0 +1,335 @@
+const { Application } = require('probot');
+
+const mergeMe = require('..');
+
+const { expect } = require('chai');
+
+const { fake } = require('sinon');
+
+
+function fixture(name) {
+  return require(`./fixtures/${name}`);
+}
+
+
+describe('merge-me', () => {
+
+  let app, github, trace;
+
+  beforeEach(() => {
+    trace = [];
+
+    app = new Application();
+
+    app.log = (msg) => {
+
+      if (typeof msg === 'string' && msg.indexOf('`context.github`') === 0) {
+        return;
+      }
+
+      trace.push(msg);
+    };
+
+    app.log.child = () => app.log;
+
+    app.log.error = app.log.debug = app.log;
+
+    app.load(mergeMe);
+
+    // mock GitHub API
+    github = {
+      repos: {
+        getCombinedStatusForRef: fake(function({ owner, repo, ref }) {
+          expect(owner).to.eql('owner');
+          expect(repo).to.eql('repo');
+
+          if (ref === 'pending') {
+            return Promise.resolve({
+              data: {
+                state: 'pending'
+              }
+            });
+          }
+
+          if (ref === 'success') {
+            return Promise.resolve({
+              data: {
+                state: 'success'
+              }
+            });
+          }
+
+          if (ref === 'pending-required-complete') {
+            return Promise.resolve({
+              data: {
+                state: 'pending',
+                statuses: [
+                  { context: 'foo', state: 'success' },
+                  { context: 'bar/a', state: 'success' },
+                  { context: 'other', state: 'pending' }
+                ]
+              }
+            });
+          }
+
+          if (ref === 'failed-required-complete') {
+            return Promise.resolve({
+              data: {
+                state: 'failed',
+                statuses: [
+                  { context: 'foo', state: 'success' },
+                  { context: 'bar/a', state: 'success' },
+                  { context: 'other', state: 'failed' }
+                ]
+              }
+            });
+          }
+
+          throw new Error(`unexpected invocation ${[ owner, repo, ref ]}`);
+        }),
+        getProtectedBranchRequiredStatusChecksContexts: fake(function({
+          owner,
+          repo,
+          branch
+        }) {
+          expect(owner).to.eql('owner');
+          expect(repo).to.eql('repo');
+
+          if (branch === 'master') {
+            return Promise.resolve({
+              data: []
+            });
+          }
+
+          if (branch === 'master-protected') {
+            return Promise.resolve({
+              data: [
+                'foo',
+                'bar'
+              ]
+            });
+          }
+
+          throw new Error(`unexpected invocation ${[ owner, repo, branch ]}`);
+        })
+      },
+      pullRequests: {
+        merge: fake(function({
+          number
+        }) {
+
+          if (number === 666) {
+            return Promise.reject({
+              code: 1,
+              message: JSON.stringify({ message: 'error' })
+            });
+          }
+
+          if (number === 405) {
+            return Promise.reject({
+              code: 405,
+              message: JSON.stringify({ message: 'error' })
+            });
+          }
+
+          return Promise.resolve({
+            data: {
+              merged: true
+            }
+          });
+        }),
+        getAll: fake(function({
+          owner,
+          repo,
+          ref
+        }) {
+
+          expect(owner).to.eql('owner');
+
+          if (ref === 'repo:with-pr') {
+            return Promise.resolve({
+              data: fixture('pulls.getAll')
+            });
+          }
+
+          if (ref === 'repo:with-pr-protected') {
+            return Promise.resolve({
+              data: fixture('pulls.getAll.protected')
+            });
+          }
+
+          if (ref === 'repo:with-pr-pending') {
+            return Promise.resolve({
+              data: fixture('pulls.getAll.pending')
+            });
+          }
+
+          return Promise.resolve({
+            data: []
+          });
+        })
+      }
+    };
+
+    app.auth = () => Promise.resolve(github);
+  });
+
+
+  describe('should integrate with hooks', function() {
+
+    describe('status', function() {
+
+      function verify(payloadName, expectedTrace) {
+
+        return async () => {
+          // given
+          const payload = fixture(payloadName);
+
+          // when
+          await app.receive({
+            name: 'status',
+            payload
+          });
+
+          // then
+          expect(trace).to.eql(expectedTrace);
+        };
+
+      }
+
+
+      it('check pending', verify('status.pending', [
+        'skipping: status == pending'
+      ]));
+
+
+      it('check without branch', verify('status.noBranch', [
+        'skipping: no branch matches ref'
+      ]));
+
+
+      it('check without PR', verify('status.noPullRequest', [
+        'checking branch master',
+        'found 0 pulls',
+        'skipping: no PR matches ref',
+      ]));
+
+
+      it('check protected with PR', verify('status.protected', [
+        'checking branch with-pr-protected',
+        'found 1 pulls',
+        'checking merge on PR #1',
+        'validating merge against branch restrictions',
+        'branch status failed',
+        'merged PR #1'
+      ]));
+
+
+      it('check unprotected with PR', verify('status', [
+        'checking branch with-pr',
+        'found 1 pulls',
+        'checking merge on PR #1',
+        'validating merge against via all status checks',
+        'branch status success',
+        'merged PR #1'
+      ]));
+
+    });
+
+
+    describe('pull_request_review.submitted', function() {
+
+      function verify(payloadName, expectedTrace) {
+
+        return async () => {
+          // given
+          const payload = fixture(payloadName);
+
+          // when
+          await app.receive({
+            name: 'pull_request_review.submitted',
+            payload
+          });
+
+          // then
+          expect(trace).to.eql(expectedTrace);
+        };
+
+      }
+
+      it('approval', verify('pullRequestReview.approval', [
+        'checking merge on PR #4',
+        'validating merge against via all status checks',
+        'branch status success',
+        'merged PR #4'
+      ]));
+
+
+      it('comment', verify('pullRequestReview.comment', [
+        'skipping: review in state comment'
+      ]));
+
+    });
+
+
+    [ 'opened', 'reopened', 'synchronize' ].forEach(function(action) {
+
+      describe(`pull_request.${action}`, function() {
+
+        function verify(payloadName, expectedTrace) {
+
+          return async () => {
+            // given
+            const payload = fixture(payloadName);
+
+            // when
+            await app.receive({
+              name: `pull_request.${action}`,
+              payload
+            });
+
+            // then
+            expect(trace).to.eql(expectedTrace);
+          };
+
+        }
+
+
+        it('with status', verify('pullRequest.synchronize', [
+          'checking merge on PR #4',
+          'validating merge against via all status checks',
+          'branch status success',
+          'merged PR #4'
+        ]));
+
+
+        it('pending statues', verify('pullRequest.pending', [
+          'checking merge on PR #4',
+          'validating merge against branch restrictions',
+          'branch status pending',
+          'skipping: ref merge rejected via status check'
+        ]));
+
+
+        it('merge fail generic', verify('pullRequest.mergeFail', [
+          'checking merge on PR #666',
+          'validating merge against via all status checks',
+          'branch status success',
+          'merge failed'
+        ]));
+
+
+        it('merge fail 405', verify('pullRequest.mergeFail.405', [
+          'checking merge on PR #405',
+          'validating merge against via all status checks',
+          'branch status success',
+          'merge #405 failed: error'
+        ]));
+
+      });
+
+    });
+
+  });
+
+});
